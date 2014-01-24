98498f61fc4964a9f2f0a8e17a571673eae448c2
daniw <dani.winz@bluewin.ch>
2014-01-24 11:30:39 +0100

    Teiler 37

---
+++ raetsel-2/source/raetsel_4.py
@@ -0,0 +1,24 @@
+import time
+
+def printresult(starttime, i, j, k, l):
+	print time.time() - starttime
+	print i
+	print j
+	print k
+	print l
+	print i + j + k + l
+	print i * j * k * l
+	print 
+
+starttime = time.time()
+
+for i in range(777/4):
+	for j in range(777/3):
+		for k in range(37,777,37):
+			if (i * j * k * (777 - i - j - k)) == 777000000:
+				for l in range(774):
+					if i + j + k + l == 777:
+						if i * j * k * l == 777000000:
+							printresult(starttime, i, j, k, l)
+
+print time.time() - starttime
